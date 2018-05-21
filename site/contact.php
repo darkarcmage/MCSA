060468d0934e6df3cf1a1877bf80be53750e2cf1
JJ <jjbotha2010@gmail.com>
2018-05-21 22:04:06 +0200

    Atom Test

--- site/contact.php
+++ site/contact.php
@@ -7,7 +7,7 @@
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Hiking Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+<meta name="keywords" content="Hiking Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700,800' rel='stylesheet' type='text/css'>
 <script src="js/jquery.min.js"></script>
@@ -18,7 +18,7 @@ width: 180px;
 padding: 10px;
 }
 .content-block{
-display: flex;
+display: flex; 
 }
 div p {color: white;}
 </style>
@@ -28,7 +28,7 @@ div p {color: white;}
 <?php include "parts/header.php"; ?>
 <!-- header -->
 <!-- contact -->
-<div class="contact-data"> 
+<div class="contact-data">
 			<div class="container">
 		       <h2>Get In Touch</h2>
 		       <p>083 845 1573</p>
@@ -41,155 +41,155 @@ div p {color: white;}
 						    	<p>Tennis Club, Rietondale Park,</p>
 						   		<p>corner of Soutpansberg Road & van der Merwe Road,</p>
 						   		<p>Pretoria</p>
-				   		
+
 				     </div>
 
 				      <div class="clearfix"></div>
 				      <h2>Committee members</h2>
-					  
-					  
-					  
+
+
+
 		<div class="news-grids">
-		
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Chair</h4>					
+					<h4>Chair</h4>
 				</div>
 				<img  src="images/committee/petrus.jpeg"></img>
 					<p>Petrus Nel <br>
 					<a href="mailto:chair@mag.mcsa.org.za">chair@mag.mcsa.org.za</a></p>
 			</div>
-			
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Vice Chair</h4>			
+					<h4>Vice Chair</h4>
 				</div>
 				<img  src="images/committee/otti.jpeg"></img>
 				<p>Otti Nesser <br>
 				<a href="mailto:vice-chair@mag.mcsa.org.za">vice-chair@mag.mcsa.org.za</a></p>
-				
+
 			</div>
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Land & Access</h4>					
+					<h4>Land & Access</h4>
 				</div>
 				<img  src="images/committee/eddie.jpeg"></img>
 					<p>Eddie Harvey <br>
 					<a href="mailto:access@mag.mcsa.org.za">access@mag.mcsa.org.za</a></p>
 			</div>
-			
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Secretary</h4>					
+					<h4>Secretary</h4>
 				</div>
-				<img  src="images/committee/robyn.jpg"></img> 
+				<img  src="images/committee/robyn.jpg"></img>
 					<p>Robyn Arnold <br>
 					<a href="mailto:secretary@mag.mcsa.org.za">secretary@mag.mcsa.org.za</a></p>
 			</div>
-			
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Treasurer</h4>					
+					<h4>Treasurer</h4>
 				</div>
 				<img  src="images/committee/desi.jpeg"></img>
 					<p>Desiree Dall <br>
 					<a href="mailto:treasurer@mag.mcsa.org.za">treasurer@mag.mcsa.org.za</a></p>
 			</div>
-			
-			
+
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Climbing</h4>					
+					<h4>Climbing</h4>
 				</div>
 				<img  src="images/committee/andrew.jpeg"></img>
 					<p>Andrew Louw <br>
 					<a href="mailto:climbing@mag.mcsa.org.za">climbing@mag.mcsa.org.za</a></p>
 			</div>
-			
-			
+
+
 			<!-- CREATE NEW ROW-->
 			<div class="clearfix"></div>
-			
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Search & Rescue</h4>					
+					<h4>Search & Rescue</h4>
 				</div>
 				<img  src="images/committee/rupert.jpeg"></img>
 					<p>Rupert Stander<br>
 					<a href="mailto:rescue@mag.mcsa.org.za">rescue@mag.mcsa.org.za</a></p>
-			</div>		
-			
+			</div>
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Training</h4>					
+					<h4>Training</h4>
 				</div>
 				<img  src="images/committee/ettiene.jpeg"></img>
 					<p>Ettiene Step <br>
 					<a href="mailto:training@mag.mcsa.org.za">training@mag.mcsa.org.za</a></p>
-			</div>	
-			
+			</div>
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Youth & Outre</h4>					
+					<h4>Youth & Outre</h4>
 				</div>
 				<img  src="images/committee/karen.jpeg"></img>
 					<p>Karen Hauptfleisch <br>
 					<a href="mailto:youth@mag.mcsa.org.za">youth@mag.mcsa.org.za</a></p>
-			</div>	
-			
+			</div>
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>News</h4>					
+					<h4>News</h4>
 				</div>
 				<img  src="images/committee/kendal.jpeg"></img>
 					<p>Kendal Smith <br>
 					<a href="mailto:news@mag.mcsa.org.za">news@mag.mcsa.org.za</a></p>
-			</div>	
-			
+			</div>
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Conservation</h4>					
+					<h4>Conservation</h4>
 				</div>
 					<img  src="images/committee/corne.jpeg"></img>
 					<p>Corne Mare <br>
 					<a href="mailto:conservation@mag.mcsa.org.za">conservation@mag.mcsa.org.za</a></p>
-			</div>	
-			
-				
+			</div>
+
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Club Evneings</h4>					
+					<h4>Club Evneings</h4>
 				</div>
 					<img  src="images/committee/odette.jpeg"></img>
 					<p>Odette Lippold<br>
 					<a href="mailto:meets@mag.mcsa.org.za">meets@mag.mcsa.org.za</a></p>
-			</div>	
+			</div>
 						<!-- CREATE NEW ROW-->
 			<div class="clearfix"></div>
-			
-			
+
+
 			<div class="col-sm-2 news-grid">
 				<div class="news-grid-text">
-					<h4>Hiking</h4>					
+					<h4>Hiking</h4>
 				</div>
 					<img  src="images/committee/frik.jpeg"></img>
 					<p>Frik Stegmann<br>
 					<a href="mailto:hiking@mag.mcsa.org.za">hiking@mag.mcsa.org.za</a></p>
-			</div>	
-			
+			</div>
+
 		</div>
-					  
 
 
 
-				      
-	
 
-<div class="contact-data"> 
+
+
+
+<div class="contact-data">
 <div class="clearfix"></div>
 <h2>Connect with a local guide</h2>
 <p> These people goes climbing almost every weekend and will love to take you with. If they are not going out they can perhaps connect you with someone in the area. </p>
-	<div class="clearfix"></div>		
+	<div class="clearfix"></div>
 	<div class="col-sm-4 news-grid">
 		<div class="mid-block content-block">
 			<div class="comImg flex">
@@ -198,11 +198,11 @@ div p {color: white;}
 			<div class="flex">
 			<h2>  JJ Botha</h2>
 			<p>Sport climber <br>
-			<a href="mailto:jjbotha2010@gmail.com">jjbotha2010@gmail.com</a></p>			
-			</div>				      
+			<a href="mailto:jjbotha2010@gmail.com">jjbotha2010@gmail.com</a></p>
+			</div>
 		</div>
-	</div>	
-				
+	</div>
+
 
 	<div class="col-sm-4 news-grid">
 		<div class="mid-block content-block">
@@ -212,8 +212,8 @@ div p {color: white;}
 			<div class="flex">
 			<h2>  Nicky Sankey</h2>
 			<p>Sport climber <br>
-			<a href="mailto:nicolajean.sankey@gmail.com">nicolajean.sankey@gmail.com</a></p>			
-			</div>				      
+			<a href="mailto:nicolajean.sankey@gmail.com">nicolajean.sankey@gmail.com</a></p>
+			</div>
 		</div>
 	</div>
 
@@ -225,11 +225,11 @@ div p {color: white;}
 			<div class="flex">
 			<h2> Bernie Theron</h2>
 			<p>Sport and Trad climber <br>
-			<a href="mailto:theronbernie@gmail.com">theronbernie@gmail.com</a></p>			
-			</div>				      
+			<a href="mailto:theronbernie@gmail.com">theronbernie@gmail.com</a></p>
+			</div>
 		</div>
 	</div>
-	
+
 	<div class="clearfix"></div>
 	<!--NEW ROW -->
 	<div class="col-sm-4 news-grid">
@@ -240,11 +240,11 @@ div p {color: white;}
 			<div class="flex">
 			<h2> Sarel Petrus</h2>
 			<p>Trad climber <br>
-			<a href="mailto:sarelpetrus@gmail.com ">sarelpetrus@gmail.com </a></p>			
-			</div>				      
+			<a href="mailto:sarelpetrus@gmail.com ">sarelpetrus@gmail.com </a></p>
+			</div>
 		</div>
 	</div>
-	
+
 	<div class="col-sm-4 news-grid">
 		<div class="mid-block content-block">
 			<div class="comImg flex">
@@ -253,13 +253,13 @@ div p {color: white;}
 			<div class="flex">
 			<h2> Desiree Dall</h2>
 			<p>Trad and Sport climber <br>
-			<a href="mailto:ddall94@gmail.com ">ddall94@gmail.com </a></p>			
-			</div>				      
+			<a href="mailto:ddall94@gmail.com ">ddall94@gmail.com </a></p>
+			</div>
 		</div>
 	</div>
-	
+
 </div>
-</div>		
+</div>
 
   <!-- contact -->
 <!-- footer -->
